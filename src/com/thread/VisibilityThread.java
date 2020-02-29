10376fadcff05e694d24b23191350406ee000283
zhanggy <158253778@qq.com>
2020-02-29 23:39:09 +0800

    内存可见性代代码

--- src/com/thread/VisibilityThread.java
+++ src/com/thread/VisibilityThread.java
@@ -26,6 +26,5 @@ public class VisibilityThread{
 /*
 有一个共享的boolean变量shutdown，起初为false，HelloThread在shutdown不为true的时候一直死循环，当shutdown为true时输出"exit hello"，main线程启动HelloThread后休息了一会，然后设置shutdown为true,最后输出"exit main"。
 期望的结果两个线程都退出，但是HelloThread永远都不会退出，也就是对于HelloThread执行流来说shutdown永远为false，即使main线程已经更改为了true。这就是内存可见性问题。
-
 在计算机系统中，除了内存、数据还会被缓存到CPU的寄存器以及各级缓存中，当访问一个变量时，可能直接从寄存器或CPU缓存中获取，而不一定到内存中去。一个线程对内存的修改，另一个线程看不到，一是修改没有及时同步到内存，二是线程根本没从内存读
  */
