3a410c3ca061f149ff845d195f6e6516f918facc
zhanggy <158253778@qq.com>
2020-05-15 01:38:26 +0800

    修改代码规范

--- src/com/thread/cllaboration/asynchronous/MyExecutor.java
+++ src/com/thread/cllaboration/asynchronous/MyExecutor.java
@@ -1,13 +1,16 @@
 package com.thread.cllaboration.asynchronous;
 
 public class MyExecutor {
-    //这个子线程执行实际的子线程，记录执行结果到result变量，异常到exception变量，执行结束后设置共享状态变量done为true，并调用notifyAll，以唤醒可能在等待结果的主线程
+    /**
+     * 这个子线程执行实际的子线程，记录执行结果到result变量，异常到exception变量，执行结束后设置共享状态变量done为true，并调用notifyAll，以唤醒可能在等待结果的主线程
+     * @param <V>
+     */
     static class ExecuteThread<V> extends Thread {
         private V result = null;
         private Exception exception = null;
         private boolean done = false;
-        private Callable<V> task;
-        private Object lock;
+        private final Callable<V> task;
+        private final Object lock;
 
         public ExecuteThread(Callable<V> task, Object lock) {
             this.task = task;
@@ -41,27 +44,29 @@ public class MyExecutor {
         }
     }
 
-    // execute启动一个线程，并返回MyFuture对象，MyFuture的get方法会堵塞等待直到线程运行结束
+    /**
+     * execute启动一个线程，并返回MyFuture对象，MyFuture的get方法会堵塞等待直到线程运行结束
+     * @param task
+     * @param <V>
+     * @return
+     */
     public <V> MyFuture<V> execute(final Callable<V> task) {
         final Object lock = new Object();
         final ExecuteThread<V> thread = new ExecuteThread<>(task, lock);
         thread.start();
-        MyFuture<V> future = new MyFuture<V>() {
-            @Override
-            public V get() throws Exception {
-                synchronized (lock) {
-                    while (!thread.isDone()) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
+        MyFuture<V> future = () -> {
+            synchronized (lock) {
+                while (!thread.isDone()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException ignored) {
 
-                        }
                     }
-                    if (thread.getException() != null) {
-                        throw thread.getException();
-                    }
-                    return thread.getResult();
                 }
+                if (thread.getException() != null) {
+                    throw thread.getException();
+                }
+                return thread.getResult();
             }
         };
         return future;
