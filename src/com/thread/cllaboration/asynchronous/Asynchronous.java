2a44bf9491f24cde73b1456792b7fd099f67973e
zhanggy <158253778@qq.com>
2020-05-15 01:38:26 +0800

    修改代码规范

--- src/com/thread/cllaboration/asynchronous/Asynchronous.java
+++ src/com/thread/cllaboration/asynchronous/Asynchronous.java
@@ -1,18 +1,21 @@
 package com.thread.cllaboration.asynchronous;
 
 
+import java.util.Random;
+
+/**
+ * @author zgy
+ */
 public class Asynchronous {
     public static void main(String[] args) {
         MyExecutor executor = new MyExecutor();
         // 子任务
-        Callable<Integer> subTask = new Callable() {
-            @Override
-            public Integer call() throws Exception {
-                // 执行异步任务
-                int millis = (int) (Math.random() * 1000);
-                Thread.sleep(millis);
-                return millis;
-            }
+        Callable<Integer> subTask = () -> {
+            // 执行异步任务
+            Random random = new Random();
+            int millis = random.nextInt(1000);
+            Thread.sleep(millis);
+            return millis;
         };
         // 异步调用，返回一个MyFuture对象
         MyFuture<Integer> future = executor.execute(subTask);
