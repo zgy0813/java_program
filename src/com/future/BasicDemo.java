7316bed7f62fc68f1c15a3b69ce44e038d8db2d1
zhanggy <158253778@qq.com>
2020-08-25 22:23:34 +0800

    简单的线程创建

--- src/com/future/BasicDemo.java
+++ src/com/future/BasicDemo.java
@@ -1,32 +1,46 @@
 package com.future;
 
-import com.thread.cllaboration.asynchronous.Callable;
 
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-
+import java.util.concurrent.*;
 
 public class BasicDemo {
-    static class Task2 implements Callable<Integer> {
-        @Override
-        public Integer call() throws Exception {
-            int sleepSeconds = new Random().nextInt(1000);
-            Thread.sleep(sleepSeconds);
-            return sleepSeconds;
-        }
-    }
 
-    public static void main(String[] args) throws InterruptedException{
-        ExecutorService executor = new SimpleExecutorService();
-        Future<Integer> future = executor.submit(new Task2());
-        Thread.sleep(100);
-        try {
-            System.out.println(future.get());
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        executor.shutdown();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        Callable<String> callable1 = () -> {
+            Thread.sleep(5000);
+            String out = "11111111111111111111111111";
+            System.out.println(out);
+            return out;
+        };
+        FutureTask futureTask1 = new FutureTask<>(callable1);
+        new Thread(futureTask1).start();
+
+        Callable<String> callable2 = () -> {
+            Thread.sleep(1000);
+            String out = "22222222222222222222222222";
+            System.out.println(out);
+            return out;
+        };
+
+        Runnable runnable1 = () -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String out = "333333333333333333333333333";
+            System.out.println(out);
+        };
+        new Thread(runnable1).start();
+
+        FutureTask futureTask2 = new FutureTask<>(callable2);
+        new Thread(futureTask2).start();
+
+        String out1 = (String) futureTask1.get();
+        System.out.println("out1="+out1);
+
+        String out2 = (String) futureTask2.get();
+        System.out.println("out2="+out2);
     }
 }
